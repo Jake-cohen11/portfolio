1c2d12419c5bc4b9adabfc511711f386744c19ee
Jake-cohen11 <51756504+Jake-cohen11@users.noreply.github.com>
2024-09-26 20:12:56 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+/*let titles = ['Web Developer', 'Web Designer', 'Graphic Designer'];
+let currentIndex = 0;
+let aSpan = document.getElementById('job-title');
+console.log("test");
+
+setInterval(() => {
+   
+   aSpan.innerHTML= titles[currentIndex];   
+   
+   currentIndex++;
+   
+   if (currentIndex === 4)
+    currentIndex = 0;
+
+}, 1000)*/
