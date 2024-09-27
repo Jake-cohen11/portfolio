73c7f554ab7b54784507e2e4a151f02497fd4b17
Jake Cohen <51756504+Jake-cohen11@users.noreply.github.com>
2024-09-27 17:51:10 -0400

    Added scrolling description funciton

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-/*let titles = ['Web Developer', 'Web Designer', 'Graphic Designer'];
-let currentIndex = 0;
-let aSpan = document.getElementById('job-title');
-console.log("test");
+document.addEventListener("DOMContentLoaded", function(event) { 
+    let words = ['Web Developer', 'Web Designer', 'Game Developer', 'Software Engineer'];
+    let i = 0;
+    let timer;
 
-setInterval(() => {
-   
-   aSpan.innerHTML= titles[currentIndex];   
-   
-   currentIndex++;
-   
-   if (currentIndex === 4)
-    currentIndex = 0;
+    function typingEffect() {
+        let word = words[i].split("");
+        var loopTyping = function() {
+            if (word.length > 0) {
+                document.getElementById('word').innerHTML += word.shift();
+            } else {
+                deletingEffect();
+                return false;
+            };
+            timer = setTimeout(loopTyping, 50);
+        };
+        loopTyping();
+    };
 
-}, 1000)*/
+    function deletingEffect() {
+        let word = words[i].split("");
+        var loopDeleting = function() {
+            if (word.length > 0) {
+                word.pop();
+                document.getElementById('word').innerHTML = word.join("");
+            } else {
+                if (words.length > (i + 1)) {
+                    i++;
+                } else {
+                    i = 0;
+                };
+                typingEffect();
+                return false;
+            };
+            timer = setTimeout(loopDeleting, 100);
+        };
+        loopDeleting();
+    };
+
+    typingEffect();
+});
